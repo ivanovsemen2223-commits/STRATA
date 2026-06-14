6d6c04d356ae47539da6625b3787cf2838ad6de5
Ivanov Semen <ivanovsemen2223@gmail.com>
2026-06-14 15:06:00 +0000

    Add Apex Arc landing site

---
+++ script.js
@@ -0,0 +1,14 @@
+// Плавное появление блоков при скролле
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((e) => {
+    if (e.isIntersecting) {
+      e.target.classList.add('in');
+      io.unobserve(e.target);
+    }
+  });
+}, { threshold: 0.1, rootMargin: '0px 0px -40px 0px' });
+
+document.querySelectorAll('.reveal').forEach((el, i) => {
+  el.style.transitionDelay = (i % 4) * 70 + 'ms';
+  io.observe(el);
+});
